c2bf491b9e961d8fb843722dc78a2334dd6fe499
Edvin Malinovskis <edvin.malinovskis@gmail.com>
2014-01-29 12:45:19 +0000

    Updated setup.py
    Switched from distutils to setuptools
    Added 'irc' as a requirement

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 setup(name='thealot',
       version='dev.1',
       author='Edvin "nCrazed" Malinovskis',
       author_email='edvin.malinovskis@gmail.com',
       url='https://github.com/nCrazed/TheAlot',
       packages=['thealot', 'thealot.plugins'],
+      install_requires=[
+          'irc',
+          ]
       )
